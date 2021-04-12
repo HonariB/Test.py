5b1d98d10aca6a031192e3b8461e564ce9a9dcf7
HonariB <79597037+HonariB@users.noreply.github.com>
2021-04-12 18:13:15 +0100

    PyCharm for Finance

--- Test.py
+++ Test.py
@@ -1,17 +1,2 @@
 print("Hello World")
-a=1
-print(a)
-b=2+2
-print(b)
-c=5+4
-print(c)
-
-print("it's 8:44pm")
-
-data= pd.read_csv("country_vaccinations.csv")
-
-
-import pandas as pd
-data=pd.read_csv("country_vaccinations.csv")
-print("data")
-print("we made some new changes")
+print("Hi")
